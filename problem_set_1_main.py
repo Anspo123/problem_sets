a7e9825033f0e1536199289742f4824c7e177cbd
Anspo123 <Anna@bnk.se>
2026-09-13 12:36:17 +0200

    Add files via upload

--- problem_set_1_main.py
+++ problem_set_1_main.py
@@ -124,16 +124,7 @@ class DataAnalysis:
 
         # OLS:
         if model.upper() == "OLS":
-            result = sm.OLS(y, x).fit(cov_type="HC1")
-
-        # Probit:
-        elif model.upper() == "PROBIT":
-            if not set(y.dropna().unique()).issubset({0, 1}):
-                raise ValueError(
-                "Probit requires a binary dependent variable."
-                )
-
-            result = sm.Probit(y, x).fit(cov_type="HC1")
+            result = sm.OLS(y, x).fit(cov_type="HC1", use_t=True)
 
         else:
             raise ValueError(f"Unknown regression model: {model}")
@@ -144,6 +135,7 @@ class DataAnalysis:
 
 
 class VisualizeData:
+    """ Visualize data."""
 
     def __init__(self, df):
 
@@ -161,8 +153,8 @@ class VisualizeData:
                      y,
                      x,
                      c_title,
-                     x_lable,
-                     y_lable,
+                     x_label,
+                     y_label,
                      output_path
     ):
 
@@ -192,8 +184,8 @@ class VisualizeData:
 
         plt.legend()
         plt.title(c_title)
-        plt.xlabel(x_lable)
-        plt.ylabel(y_lable)
+        plt.xlabel(x_label)
+        plt.ylabel(y_label)
         plt.xticks(rotation=45)
         plt.tight_layout()
         plt.savefig(output_path)
@@ -229,6 +221,7 @@ class VisualizeData:
 # MAIN FUNCTION
 # -----------------------
 def main():
+    """ Run the program. """
 
     # File paths:
     input_path = Path("Python") / "5304" / "problem_set_1" / "input"
@@ -237,6 +230,12 @@ def main():
     female_data_path = input_path / "data_female.txt"
     male_data_path = input_path / "data_male.txt"
 
+    if not female_data_path.exists():
+        raise FileNotFoundError(f"File not found: {female_data_path}")
+
+    if not male_data_path.exists():
+        raise FileNotFoundError(f"File not found: {male_data_path}")
+
     final_data_path = output_path / "final_dataset.csv"
 
     # Load data:
@@ -244,17 +243,13 @@ def main():
     male_df = import_txt_file(male_data_path)
 
     # Clean data:
-    try:
-        prep_fem = DataPreparation(female_df)
-        female_df = prep_fem.check_dataset()
-        female_df_clean = prep_fem.clean_data()
+    prep_fem = DataPreparation(female_df)
+    female_df = prep_fem.check_dataset()
+    female_df_clean = prep_fem.clean_data()
 
-        prep_male = DataPreparation(male_df)
-        male_df = prep_male.check_dataset()
-        male_df_clean = prep_male.clean_data()
-
-    except ValueError:
-        return
+    prep_male = DataPreparation(male_df)
+    male_df = prep_male.check_dataset()
+    male_df_clean = prep_male.clean_data()
 
     # Prepare final dataset:
     finish_data = FinishDataset(female_df_clean, male_df_clean)
@@ -273,25 +268,23 @@ def main():
     using an appropriate regression model.
     """
 
-    try: 
-        # Load variables:
-        data_analysis = DataAnalysis(final_df)
-        y1, x1 = data_analysis.load_data(
-            yvar ="steps",
-            xvars =["female"]
-        )
+    # Load variables:
+    data_analysis = DataAnalysis(final_df)
+    y1, x1 = data_analysis.load_data(
+        yvar ="steps",
+        xvars =["female"]
+    )
 
-        # Regression ("OLS", y1, x1):
-        ols_result = data_analysis.regression("OLS", y1, x1)
+    # Regression ("OLS", y1, x1):
+    print()
+    print("TASK 1:")
+    ols_result = data_analysis.regression("OLS", y1, x1)
 
-        # Save as text:
-        visualize_data.save_results_as_text(
-            ols_result,
-            output_path / "ols_table.txt"
-        )
-
-    except ValueError:
-        return
+    # Save as text:
+    visualize_data.save_results_as_text(
+        ols_result,
+        output_path / "table_task_1.txt"
+    )
 
     # -----------------------
     # 2. The gender gap. 
@@ -300,28 +293,25 @@ def main():
     Jonas recommends adding income and resting heart rate as controls.
     """
 
-    try:
         # Load variables:
-        y2, x2 = data_analysis.load_data(
-            yvar ="steps",
-            xvars =["female",
-                    "income",
-                    "resting_heart_rate"
-            ]
-        )   
+    y2, x2 = data_analysis.load_data(
+        yvar ="steps",
+        xvars =["female",
+                "income",
+                "resting_heart_rate"
+        ]
+    )   
 
-        # Regression ("MULTIPLE OLS", y2, x2):
-        ols_result_controls = data_analysis.regression("OLS", y2, x2)
+    # Regression ("MULTIPLE OLS", y2, x2):
+    print()
+    print("TASK 2:")
+    ols_result_controls = data_analysis.regression("OLS", y2, x2)
 
-        # Save as text:
-        visualize_data.save_results_as_text(
-            ols_result_controls,
-            output_path / "ols_controls_table.txt"
-        )
-
-    except ValueError:
-        return
-
+    # Save as text:
+    visualize_data.save_results_as_text(
+        ols_result_controls,
+        output_path / "table_task_2.txt"
+    )
 
     # -----------------------
     # 3. Steps and BMI. 
@@ -331,36 +321,40 @@ def main():
     daily steps among women.
     """
 
-    women = final_df[final_df["female"] == 1]
-    women_analysis = DataAnalysis(women)
+    women_df = final_df[final_df["female"] == 1]
+    women_analysis = DataAnalysis(women_df)
 
-    try:
-
-        y3, x3 = women_analysis.load_data(
+    y3, x3 = women_analysis.load_data(
         yvar="steps",
         xvars=["bmi"]
-        )
+    )
 
-        # Regression ("T-TEST", y2, x2)
-        ols_result_women = data_analysis.regression("OLS", y3, x3)
+    # Regression ("OLS", y3, x3)
+    ols_result_women = women_analysis.regression("OLS", y3, x3)
 
-        # Find t-value:
-        test = ols_result_women.t_test("bmi = 0")
+    # Save as text:
+    visualize_data.save_results_as_text(
+        ols_result_women,
+        output_path / "table_task_3.txt"
+    )
 
-        coefficient = ols_result_women.params["bmi"]
-        two_sided_p = float(test.pvalue)
+    # Find t-value:
+    test = ols_result_women.t_test("bmi = 0")
 
-        if coefficient < 0:
-            one_sided_p = two_sided_p / 2
+    coefficient = ols_result_women.params["bmi"]
+    two_sided_p = float(test.pvalue)
 
-        else:
-            one_sided_p = 1 - two_sided_p / 2
+    if coefficient < 0:
+        one_sided_p = two_sided_p / 2
 
-        print()
-        print(f"One sided p-vale: {one_sided_p:.4f}")
+    else:
+        one_sided_p = 1 - two_sided_p / 2
 
-    except ValueError:
-        return
+    print()
+    print("TASK 3:")
+    print("=" * 30)
+    print(f"One sided p-value: {one_sided_p:.4f}")
+    print("=" * 30)
 
     # -----------------------
     # 4. Steps and BMI. 
@@ -370,29 +364,37 @@ def main():
     regressors.
     """
 
-    try:
+    # Load variables:
+    y4, x4 = data_analysis.load_data(
+        yvar ="steps",
+        xvars =["female",
+                "bmi",
+                "height",
+                "weight"
+        ]
+    )
 
-        # Load variables:
-        y4, x4 = data_analysis.load_data(
-            yvar ="steps",
-            xvars =["female",
-                    "bmi",
-                    "height",
-                    "weight"
-            ]
-        )
+    # Regression ("MULTIPLE OLS", y4, x4):
+    print()
+    print("TASK 4:")
+    ols_result_controls_2 = data_analysis.regression("OLS", y4, x4)
 
-        # Regression ("MULTIPLE OLS", y2, x2):
-        ols_result_controls_2 = data_analysis.regression("OLS", y4, x4)
+    # Save as text:
+    visualize_data.save_results_as_text(
+        ols_result_controls_2,
+        output_path / "table_task_4.txt"
+    )
 
-        # Save as text:
-        visualize_data.save_results_as_text(
-            ols_result_controls_2,
-            output_path / "ols_controls_2_table.txt"
-        )
-
-    except ValueError:
-        return
+    # Create scatterplot:
+    visualize_data.create_chart(
+                "SCATTERPLOT",
+                final_df["steps"], # y-variable
+                final_df["bmi"], # x-variable
+                "Figure 1: Daily Steps and BMI",
+                "BMI [kg/m2]",
+                "Daily Steps",
+                output_path / "scatterplot.png"
+    )
 
     # -----------------------
     # 5. Other patterns. 
@@ -402,44 +404,42 @@ def main():
     the analyses above may have missed?
     """
 
-    # Make SCATTER MATRIX:
-    visualize_data.create_matrix(
+    # Make scatter matrix:
+    variable_labels = {
+    "steps": "Daily Steps",
+    "bmi": "BMI [kg/m²]",
+    "height": "Height [cm]",
+    "weight": "Weight [kg]",
+    "income": "Annual Income [EUR]",
+    "resting_heart_rate": "Heart Rate [bpm]",
+    "shoe_size": "Shoe Size [EU]"
+    }
+
+    labelled_df = final_df.rename(columns=variable_labels)
+
+    labelled_visualize_data = VisualizeData(labelled_df)
+
+    labelled_visualize_data.create_matrix(
         [
-        "steps",
-        "bmi",
-        "height",
-        "weight",
-        "income",
-        "resting_heart_rate",
-        "shoe_size",
+            "Daily Steps",
+            "BMI [kg/m²]",
+            "Height [cm]",
+            "Weight [kg]",
+            "Annual Income [EUR]",
+            "Heart Rate [bpm]",
+            "Shoe Size [EU]",
         ],
-        "Scatter Matrix",
+        "Figure 2: Scatter Matrix of All Variables",
         output_path / "scatter_matrix.png"
     )
-
-    # -----------------------
-    # 5. Added bonus.
-    # -----------------------
-
-    try:
-
-        visualize_data.create_chart(
-                    "SCATTERPLOT",
-                    final_df["steps"], # x-variable
-                    final_df["bmi"], # y-variable
-                    "BMI and Average Daily Steps",
-                    "BMI",
-                    "Steps",
-                    output_path / "scatterplot.png"
-        )
-
-    except ValueError as error:
-        print(f"Chart error: {error}")
-        return
 
 
 # -----------------------
 # MAIN GUARD
 # -----------------------
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+
+    except (FileNotFoundError, ValueError, KeyError, OSError) as error:
+        print(f"Error: {error}")
